2426e2af7e7d61dc055eb1296dba0d47368ef459
adminManoj <deshwal.03@gmail.com>
2018-02-09 17:58:05 +0530

    Add project files.

---
+++ MVCTutorial/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MVCTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Manoj Deshwal";
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Name = "About Manoj Deshwal";
+            return View();
+        }
+        public ActionResult Details()
+        {
+            List<Employee> listEmployee = new List<Employee>();
+            Employee emp = new Employee();
+            listEmployee.Add(new Employee { ID = 1, Name = "Ricky", Dept = "Sports", Address = "Australia" });
+            listEmployee.Add(new Employee { ID = 2, Name = "Mark", Dept = "IT", Address = "England" });
+            listEmployee.Add(new Employee { ID = 3, Name = "MSD", Dept = "Cricket", Address = "India" });
+            listEmployee.Add(new Employee { ID = 4, Name = "Virat", Dept = "Cricket", Address = "India" });
+
+            return View(listEmployee);
+        }
+
+    }
+}
---
+++ MVCTutorial/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTutorial.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Dept { get; set; }
+        public string Address { get; set; }
+    }
+}
